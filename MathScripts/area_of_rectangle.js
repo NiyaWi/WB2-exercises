da80b01578537908d1c8a7c25173432c3bd87529
NiyaWi <Niyaw144@gmail.com>
2023-10-17 14:52:42 -0400

    exercises 1 and 2

---
+++ MathScripts/area_of_rectangle.js
@@ -0,0 +1,9 @@
+"user scripts"
+
+var length=5;
+var height=4
+var base=6;
+
+var length + height + base= rectangle;
+
+console.log("The area of the rectangle is" + " " + rectangle);
---
+++ MathScripts/net_worth.js
@@ -0,0 +1,6 @@
+"user scripts"
+ 
+var assets = 102457;
+var debt = 35000;
+var networth= assets - debt;
+ console.log("Your networth is" + " " + networth)
