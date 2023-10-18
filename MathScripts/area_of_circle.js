11d53819d3403407b3d85099f284ffbd8b5a3c4e
NiyaWi <Niyaw144@gmail.com>
2023-10-18 14:17:52 -0400

    exercise 1 pt 4

--- MathScripts/area_of_circle.js
+++ MathScripts/area_of_circle.js
@@ -2,6 +2,6 @@
 
 var pi= Math.PI;
 var radius=45;
-var areaOfCircle = pi*radius**2;
+var areaOfCircle = pi*Math.pow(radius,2);
 
 console.log ("The area of a circle with radius" + " " + radius + " " +  "is" + " " + areaOfCircle);
---
+++ MathScripts/distance.js
@@ -0,0 +1,9 @@
+"user script"
+
+var pOneX=4;
+var pOneY=6;
+var ptwoX=6;
+var ptwoY=9;
+var distance=Math.sqrt(Math.pow(ptwoX-pOneX,2)+Math.pow(ptwoY-pOneY,2));
+
+ console.log(distance);
